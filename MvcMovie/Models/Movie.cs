04ba6b63bb9b75c958cd0ae8285358f4450dedca
longquan98@gmail.com <longquan98@gmail.com>
2024-03-15 15:08:14 -0400

    Controller and VIews

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,13 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
